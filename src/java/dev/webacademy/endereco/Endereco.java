9a811f7ba5cb8ebcd8b488efd329e3f6973a2e90
Diego Santos <santos.diegomendonca@gmail.com>
2018-12-22 13:37:36 -0200

    entidade de persistência endereco

---
+++ src/java/dev/webacademy/endereco/Endereco.java
@@ -0,0 +1,109 @@
+package dev.webacademy.endereco;
+
+import dev.webacademy.generico.Pessoa;
+import java.io.Serializable;
+
+public class Endereco implements Serializable {
+
+    private int id;
+    private Pessoa pessoa;
+    private String cep;
+    private String logradouro;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String complemento;
+
+    public Endereco() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String Uf) {
+        this.uf = Uf;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 61 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Endereco other = (Endereco) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ src/java/dev/webacademy/endereco/EnderecoDAO.java
@@ -0,0 +1,58 @@
+package dev.webacademy.endereco;
+import dev.webacademy.aluno.Aluno;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class EnderecoDAO {
+
+    private Connection conexao = null;
+
+    public EnderecoDAO(Connection connection) {
+        this.conexao = connection;
+    }
+
+    public boolean inserir(Endereco endereco) throws SQLException {
+        boolean isSalvo = false;
+        PreparedStatement ps = null;
+        String fkPessoa = "";
+        String nomeTabela = "";
+        StringBuilder sql = new StringBuilder();
+        try {
+            if (this.conexao != null && !this.conexao.isClosed()) {
+                if (endereco.getPessoa() != null) {
+                    if (endereco.getPessoa() instanceof Aluno) {
+                        nomeTabela += "endereco_professor";
+                        fkPessoa += "id_professor";
+                    } /*else if (endereco.getPessoa() instanceof Professor) {
+                        nomeTabela += "endereco_aluno";
+                        fkPessoa += "id_aluno";
+                    }*/
+                    sql.append("INSERT INTO ").append(nomeTabela).append("(");
+                    sql.append(fkPessoa).append(",cep,logradouro,bairro,cidade,uf,complemento");
+                    sql.append(") ");
+                    sql.append("VALUES(?,?,?,?,?,?,?);");
+                    ps = this.conexao.prepareStatement(sql.toString());
+                    ps.setInt(1, endereco.getPessoa().getId());
+                    ps.setString(2, endereco.getCep());
+                    ps.setString(3, endereco.getLogradouro());
+                    ps.setString(4, endereco.getBairro());
+                    ps.setString(5, endereco.getCidade());
+                    ps.setString(6, endereco.getUf());
+                    ps.setString(7, endereco.getComplemento());
+                    if (ps.executeUpdate() == 1) {
+                        isSalvo = true;
+                    }
+                }
+            }
+        } catch (SQLException ex) {
+            throw ex;
+        } finally {
+            fkPessoa = null;
+            nomeTabela = null;
+            sql = null;
+            ps = null;
+        }
+        return isSalvo;
+    }
+}
